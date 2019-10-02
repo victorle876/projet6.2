acc52a5253503f49c947cfc1dea7d91efc75b3e4
victorle876 <40641046+victorle876@users.noreply.github.com>
2019-10-02 21:57:01 +0200

    Delete CotationDaoInterface.java

--- git/projet62/ticket/ticket-consumer/src/main/java/org/exemple/demo/consumer/daoInterface/CotationDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package org.exemple.demo.consumer.daoInterface;
-
-import java.io.Serializable;
-import java.util.List;
- 
-public interface CotationDaoInterface extends AbstractDao<Cotation, String> {
- 
-     
-}
