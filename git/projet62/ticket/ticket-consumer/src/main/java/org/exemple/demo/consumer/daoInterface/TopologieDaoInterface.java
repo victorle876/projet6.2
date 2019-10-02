93e1115dea2cb4a2949333a77b2ec6341e0b1ba4
victorle876 <40641046+victorle876@users.noreply.github.com>
2019-10-02 21:58:00 +0200

    Delete TopologieDaoInterface.java

--- git/projet62/ticket/ticket-consumer/src/main/java/org/exemple/demo/consumer/daoInterface/TopologieDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package org.exemple.demo.consumer.daoInterface;
-import java.io.Serializable;
-import java.util.List;
-import org.exemple.demo.consumer.dao.AbstractDao;
-import org.exemple.demo.entities.*;
- 
-public interface TopologieDaoInterface extends AbstractDao<Topologie, String> {
- 
-}
